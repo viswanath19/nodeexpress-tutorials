64dc7bb53a7bfa06703845beee37bb929fbf18d0
Viswanath <allamrajuviswanath@gmail.com>
2023-06-28 22:36:27 +0530

    resolving s3 bucket credentials expose issue

--- services/uploadDataObjects.js
+++ services/uploadDataObjects.js
@@ -1,10 +1,14 @@
 const AWS = require('aws-sdk');
 
-AWS.config.loadFromPath('./config.json');
-
-const s3 = new AWS.S3();
-
 export const uploadDataObjects = (req,res,next) => {
+
+    AWS.config.update({
+        accessKeyId : process.env.AWS_ACCESS_KEY,
+        secretAccessKey : process.env.AWS_SECRET_KEY,
+        region : process.env.AWS_REGION
+    });
+
+    const s3 = new AWS.S3();
 
     const data = {Bucket:'myawsnodejsbucket',Key:`${req.body.objectName}-${(new Date()).toLocaleTimeString()}.text`,Body:JSON.stringify(req.body)}
     
